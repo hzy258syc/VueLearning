b44a48230c974b83c774b196a163efd136483a0e
hzy258syc <1183246540@qq.com>
2018-08-25 22:35:46 +0800

    vue练习

---
+++ ES6的常用语法/导入包/test1.js
@@ -0,0 +1,10 @@
+// from test2 import name age aa
+// import test2
+// test2.name
+// test2.age
+// test2.aa()
+
+
+import {name, aa} from "./test2";
+
+
---
+++ ES6的常用语法/导入包/test2.js
@@ -0,0 +1,8 @@
+let name = "yuan";
+let age = 99;
+function aa() {
+    return 1
+}
+
+
+export {name, aa}
